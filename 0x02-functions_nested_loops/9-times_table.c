f5387b14a6f3cc6c5b1792e6f475e4455fe4fb8f
karey njeri <kareynjeri@gmail.com>
2022-03-15 23:31:29 +0300

    added file 9-main.c ,9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+/*
+ * File: 9-times_table.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * times_table - prints the 9 times table,
+ * starting with 0
+ *
+ */
+void times_table(void)
+{
+	int number, mult, product;
+
+	for (number = 0; number <= 9; number++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			product = number * mult;
+
+			if (product <= 9)
+				_putchar(' ');
+			else
+				_putchar((product / 10) + '0');
+			_putchar((product % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
