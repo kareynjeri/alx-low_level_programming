d9421334ef3d6adae8df2d455d3e427c1d88c599
karey njeri <kareynjeri@gmail.com>
2022-03-16 00:58:48 +0300

    added file new file:   101-natural.c         modified:   3-islower         modified:   3-islower.c         modified:   3-main.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,20 +1,22 @@
-/*
- * File: 3-islower.c
- * Auth: Samuel Kioko
- */
 #include "main.h"
+
 /**
- * _islower - checks for lowercase character.
- * @c: The character to be checked.
- *
- * Return: Return 1 if case is lowercase, otherwise 0
+ * _islower - Check main
+ * @c: An input character
+ * Description: function uses _putchar function to print
+ * alphabet in lowercase 10 times
+ * Return: 1 if is lowercase or 0 if is uppercase
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-		return (1);
-	else
-		return (0);
-		_putchar('\n');
-}
+	char i;
+	int lower = 0;
 
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i == c)
+			lower = 1;
+	}
+
+	return (lower);
+}
