17eea27ec1b8fc016f959303fd9721e25680a35d
karey njeri <kareynjeri@gmail.com>
2022-03-15 21:33:05 +0300

    Modified file 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,27 @@
-#include <studio.h>
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: kare
+ *
+ */
+#include "main.h"
+/**
+ * print_alphabet_x10 -  prints 10 times the alphabet,
+ * in lowercase, followed by a new line.
+ *
+ */
+void print_alphabet_x10(void)
+{
+	int counter;
+	char x;
+
+	for (counter = 0; counter <= 9; counter++)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+	}
+
+}
+
