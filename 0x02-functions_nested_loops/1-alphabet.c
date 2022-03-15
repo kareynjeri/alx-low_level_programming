aa574b913c60937a42b4608265206c3bc72d9638
karey njeri <kareynjeri@gmail.com>
2022-03-15 19:20:58 +0300

    added file

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -10,11 +10,11 @@
  */
 void print_alphabet(void)
 {
-    char i;
+char i;
 
-    for (i = 'a'; i <= 'z'; i++)
-    {
-        _putchar(i);
-    }
-    _putchar('\n');
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
 }
