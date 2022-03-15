b5d6df05ad31729c6963a577aea5a70427f714c0
karey njeri <kareynjeri@gmail.com>
2022-03-15 21:51:05 +0300

    modified file  3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+/*
+ * File: 3-islower.c
+ * Auth: Samuel Kioko
+ */
+#include "main.h"
+/**
+ * _islower - checks for lowercase character.
+ * @c: The character to be checked.
+ *
+ * Return: Return 1 if case is lowercase, otherwise 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+		_putchar('\n');
+}
