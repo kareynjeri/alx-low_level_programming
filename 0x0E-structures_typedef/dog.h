aa0f7a97aa07a39ae8d29b40b5b09155c9e4ca5f
karey njeri <kareynjeri@gmail.com>
2022-04-12 12:46:54 +0300

    modified dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,16 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-*struct dog:data type structure for a variable dog with a name age and owner./c
-
+*struct dog-represents a dog with a name age and owner
 *@name:contains name of variable dog
 *@age:contains age of variable dog
 *@owner:contains owner of variable dog
 **/
+
 struct dog
 {
 char *name;
 float age;
 char *owner;
 };
+
+typedef struct dog dog_t;
 #endif
