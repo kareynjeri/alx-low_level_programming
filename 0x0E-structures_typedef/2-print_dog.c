b6a12da5cea36c70c680fb5af2f588d14a5c7a09
karey njeri <kareynjeri@gmail.com>
2022-04-12 21:06:23 +0300

    deleted the test files

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,16 +1,24 @@
 #include "dog.h"
-#include <stdlib.h>
 #include <stdio.h>
 
 /**
-*print_dog - prints a struct dog
-*@d:memory address to a struct dog
-**/
+ * print_dog - prints a dog
+ *
+ * @d: dog to print
+ *
+ * Return: void
+ */
 void print_dog(struct dog *d)
 {
-if (d == NULL)
-return;
-printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
-printf("Age: %6f\n", d->age);
-printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+	if (d == 0)
+		return;
+	if (d->name == 0)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == 0)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
 }
