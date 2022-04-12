8fd76b8bae7fb03dd80d68438143e90fea343d65
karey njeri <kareynjeri@gmail.com>
2022-04-12 11:00:19 +0300

    intial commit for 0x0D-preprocessor

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+*struct dog:data type structure for a variable dog
+*@name:contains name of variable dog
+*@age:contains age of variable dog
+*@owner:contains owner of variable dog
+**/
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
